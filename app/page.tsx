c531c826071918b5297f04f81afa84dda0ad522d
Milad <milad@axorizen.com>
2026-07-22 16:07:21 +0330

    redirect home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,12 @@
-import AppShell from "@/components/app-shell";
-import AppHeader from "@/components/app-header";
-import Card from "@/components/card";
-import ProgressBar from "@/components/progress-bar";
+import { redirect } from "next/navigation";
+import { getSession } from "@/lib/session";
 
+export default async function HomePage() {
+  const session = await getSession();
 
-export default function Home(){
+  if (session) {
+    redirect("/dashboard");
+  }
 
-return (
-
-<AppShell>
-
-<AppHeader/>
-
-
-<Card>
-
-<h1 className="
-text-2xl
-font-bold
-">
-Welcome back 👋
-</h1>
-
-
-<p className="
-mt-2
-text-gray-500
-">
-Today Goal: 10 words
-</p>
-
-
-<div className="mt-5">
-
-<ProgressBar value={40}/>
-
-</div>
-
-
-</Card>
-
-
-</AppShell>
-
-)
-
+  redirect("/login");
 }
